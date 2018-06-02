996889803034f4872a65eabbcd24c84989b36b0a
Souvik Roy <souvik0789@gmail.com>
2018-06-02 11:55:46 +0530

    Add view layouts. Make user bank accounts table

--- ADMIN/user/views/all.php
+++ ADMIN/user/views/all.php
@@ -5,35 +5,46 @@
   $sql = "SELECT * FROM tbl_user WHERE 1 order by user_id DESC";
   $query = $connect->query($sql);
 ?>
-<table class="table table-striped table-hover">
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Social No.</th>
-      <th>Phone No.</th>
-      <th>PayTm</th>
-      <th>Points</th>
-      <th>Last Question</th>
-      <th>Active</th>
-    </tr>
 
-    <?php
-        while($userData = $query->fetch_assoc()) {
-          $user = new User($userData);
-    ?>
-          <tr>
-            <td> <?php echo $user->name; ?> </td>
-            <td> <?php echo $user->email; ?> </td>
-            <td> <?php echo $user->social_no; ?> </td>
-            <td> <?php echo $user->phone_no; ?> </td>
-            <td> <?php echo $user->paytm; ?> </td>
-            <td> <?php echo $user->point; ?> </td>
-            <td> <?php echo $user->last_question; ?> </td>
-            <td> <?php echo $user->active; ?> </td>
-          </tr>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Latest Users</h3>
+  </div>
+  <div class="panel-body">
+    <table class="table table-striped table-hover">
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Social No.</th>
+          <th>Phone No.</th>
+          <th>PayTm</th>
+          <th>Points</th>
+          <th>Last Question</th>
+          <th>Active</th>
+        </tr>
 
-    <?php } ?>
+        <?php
+            while($userData = $query->fetch_assoc()) {
+              $user = new User($userData);
+        ?>
+              <tr>
+                <td> <?php echo $user->name; ?> </td>
+                <td> <?php echo $user->email; ?> </td>
+                <td> <?php echo $user->social_no; ?> </td>
+                <td> <?php echo $user->phone_no; ?> </td>
+                <td> <?php echo $user->paytm; ?> </td>
+                <td> <?php echo $user->point; ?> </td>
+                <td> <?php echo $user->last_question; ?> </td>
+                <td> <?php echo $user->active; ?> </td>
+              </tr>
 
-  </table>
+        <?php
+              unset($user);
+            }
+        ?>
 
-<?php $connect->close(); ?>
+      </table>
+
+    <?php $connect->close(); ?>
+  </div>
+</div>
---
+++ ADMIN/views/layout/partials/_head.php
@@ -0,0 +1,10 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin Area | Dashboard</title>
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <script src="http://cdn.ckeditor.com/4.6.1/standard/ckeditor.js"></script>
+</head>
