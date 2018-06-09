aa7187485a539b0ca52c7e2fc5a44f33a617dbee
Souvik Roy <souvik0789@gmail.com>
2018-06-10 00:20:49 +0530

    Add new files

---
+++ ADMIN/user-details.php
@@ -0,0 +1,54 @@
+<?php
+require_once('check_user_session.php');
+require_once('../ADMIN/config.php');
+require_once('../UserService.class.php');
+
+if (isset($_GET['id']) && !empty($_GET['id']) && intval($_GET['id']) > 0) {
+  $user_id = intval($_GET['id']);
+}
+
+$userService = new UserService();
+$userData = $userService->setUser($user_id);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <?php include_once('views/layout/partials/_head.php'); ?>
+  <body>
+
+    <?php include_once('views/layout/partials/_nav.php'); ?>
+
+    <header id="header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10">
+            <h1>
+              <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+              Details of <small><?php echo $userService->user->name; ?></small></h1>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+
+              <?php include_once('user/views/bank-accounts.php'); ?>
+
+              <?php include_once('user/views/bank-transactions.php'); ?>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php
+        $connect->close();
+        include_once('views/layout/partials/_footer.php');
+        include_once('views/layout/partials/_scripts.php');
+    ?>
+
+  </body>
+</html>
---
+++ ADMIN/user/views/bank-transactions.php
@@ -0,0 +1,38 @@
+<?php $transactions = $userService->getBankTransactions(); ?>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Bank Transactions</h3>
+  </div>
+  <div class="panel-body">
+    <table class="table table-striped table-hover">
+        <tr>
+          <th>Request Date</th>
+          <th>Request Amount</th>
+          <th>Transaction Date</th>
+          <th>Transaction Amount</th>
+          <th>Transaction Status</th>
+          <th>Comment</th>
+        </tr>
+
+        <?php
+          if (!empty($bankAccount)) {
+            foreach ($transactions as $transaction) {
+        ?>
+          <tr>
+            <td> <?php echo $transaction['request_date']; ?> </td>
+            <td> <?php echo $transaction['request_amount']; ?> </td>
+            <td> <?php echo $transaction['transaction_date']; ?> </td>
+            <td> <?php echo $transaction['transaction_amount']; ?> </td>
+            <td> <?php echo $transaction['transaction_status']; ?> </td>
+            <td> <?php echo $transaction['comment']; ?> </td>
+          </tr>
+      <?php
+          }
+        }
+      ?>
+
+      </table>
+
+  </div>
+</div>
